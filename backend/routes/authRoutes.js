e75d74b380f35b5136abf00d19c6a249ceb3d900
David Okoth <138779368+Davey0076@users.noreply.github.com>
2024-11-02 16:53:57 +0300

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signup, login } = require('../controllers/authController'); // Importing functions from authController
+const { signup, login } = require('../controllers/authController'); 
 
 const router = express.Router();
 
