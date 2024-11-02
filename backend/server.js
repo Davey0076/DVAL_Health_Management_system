8f13cbf631ec12783e2b3518e88e269d4aec62c8
David Okoth <138779368+Davey0076@users.noreply.github.com>
2024-11-02 16:56:00 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const pool = require('./config/db'); // Database connection
-const authRoutes = require('./routes/authRoutes'); // Import the authentication routes
+const authRoutes = require('./routes/authRoutes');
 const cors = require('cors')
 
 
 const app = express();
-app.use(express.json()); // Middleware to parse JSON requests
+app.use(express.json());
 app.use(cors())
-// Use the auth routes with /auth prefix
-app.use('/auth', authRoutes); // This adds /auth/signup and /auth/login routes
+app.use('/auth', authRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
